468327e73bba25ea79a7828cf940408f65c5bb30
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-12-22 19:49:37 +0300

    Added the game timer and implemented the game logic

--- index.js
+++ index.js
@@ -32,3 +32,24 @@ let score = 0;
 let currentQuestion = 0;
 let gameOver = false;
 
+
+
+const GAME_TIME = 30000;
+
+function startTimer() {
+  setTimeout(() => {
+    gameOver = true;
+    console.log("\nTime's up!");
+    endGame();
+  }, GAME_TIME);
+}
+
+function startGame() {
+  console.log("Welcome to the Trivia Game!!!!!");
+  console.log("You have 30 seconds to answer as many questions as possible.");
+
+  startTimer();
+  askQuestion();
+}
+
+function askQuestion() {
